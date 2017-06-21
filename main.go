34cc8f1f6360fca5a647a792d43edeedb74dfeb4
masa23 <masa23@gmail.com>
2017-06-21 19:29:24 +0900

    update config load

--- main.go
+++ main.go
@@ -32,19 +32,21 @@ type ConfigVrrp struct {
 }
 
 type ConfigLvs struct {
-	Name     string          "yaml:`name`"
-	Port     uint16          "yaml:`port`"
-	Address  string          "yaml:`address`"
-	Schedule string          "yaml:`schedule`"
-	Type     string          "yaml:`type`"
-	Servers  []ConfigServers "yaml:`servers`"
+	Name     string         "yaml:`name`"
+	Port     uint16         "yaml:`port`"
+	Address  string         "yaml:`address`"
+	Schedule string         "yaml:`schedule`"
+	Type     string         "yaml:`type`"
+	Servers  []ConfigServer "yaml:`servers`"
 }
 
-type ConfigServers struct {
+type ConfigServer struct {
 	Port    uint16 "yaml:`port`"
 	Address string "yaml:`address`"
 	Weight  uint32 "yaml:`weight`"
 }
+
+var conf Config
 
 func main() {
 	var configfile string
@@ -56,9 +58,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	var conf Config
 	err = yaml.Unmarshal(buf, &conf)
-	pp.Println(&conf)
 
 	// ログ
 	logFile, err := os.OpenFile(conf.LogFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
@@ -75,42 +75,120 @@ func main() {
 		ltsvlog.Logger.Err(ltsvlog.Err(err).String("msg", "faild libipvs load").Stack(""))
 	}
 
-	// IPVS初期化
-	if err := ipvs.Flush(); err != nil {
-		ltsvlog.Logger.Err(ltsvlog.Err(err).String("msg", "faild to ipvs initialize").Stack(""))
+	ConfigReload(ipvs)
+}
+
+func ConfigReload(ipvs libipvs.IPVSHandle) {
+	ipvsServices, err := ipvs.ListServices()
+	if err != nil {
+		panic(err)
 	}
 
-	// serviceの設定
-	for _, service := range conf.Lvs {
-		svc := libipvs.Service{
-			Address:       net.ParseIP(service.Address),
+	var exist bool
+
+	// 不要な設定を削除
+	for _, ipvsService := range ipvsServices {
+		var serviceConf ConfigLvs
+		exist = false
+		for _, serviceConf = range conf.Lvs {
+			if ipvsService.Address.Equal(net.ParseIP(serviceConf.Address)) {
+				exist = true
+				break
+			}
+		}
+		if exist {
+			ipvsDests, err := ipvs.ListDestinations(ipvsService)
+			if err != nil {
+				ltsvlog.Logger.Err(ltsvlog.Err(err).String("msg",
+					"faild get serverIpvs list "+ipvsService.Address.String()).Stack(""))
+			}
+			for _, ipvsDest := range ipvsDests {
+				exist = false
+				for _, server := range serviceConf.Servers {
+					if ipvsDest.Address.Equal(net.ParseIP(server.Address)) {
+						exist = true
+						break
+					}
+				}
+				if !exist {
+					err := ipvs.DelDestination(ipvsService, ipvsDest)
+					if err != nil {
+						ltsvlog.Logger.Err(ltsvlog.Err(err).String("msg", "faild delete Destination").Stack(""))
+					}
+				}
+			}
+		} else {
+			err := ipvs.DelService(ipvsService)
+			if err != nil {
+				ltsvlog.Logger.Err(ltsvlog.Err(err).String("msg", "faild delete "+ipvsService.Address.String()).Stack(""))
+			}
+			ltsvlog.Logger.Info().String("msg", "delete serviceIpvs "+ipvsService.Address.String()).Log()
+		}
+	}
+
+	// 設定追加 更新
+	for _, serviceConf := range conf.Lvs {
+		exist = false
+		var ipvsService *libipvs.Service
+		for _, ipvsService = range ipvsServices {
+			if ipvsService.Address.Equal(net.ParseIP(serviceConf.Address)) {
+				exist = true
+				break
+			}
+		}
+		service := libipvs.Service{
+			Address:       net.ParseIP(serviceConf.Address),
 			AddressFamily: syscall.AF_INET,
 			Protocol:      libipvs.Protocol(syscall.IPPROTO_TCP),
-			Port:          service.Port,
+			Port:          serviceConf.Port,
 			SchedName:     libipvs.RoundRobin,
 		}
-
-		if err := ipvs.NewService(&svc); err != nil {
-			panic(err)
+		if !exist {
+			if err := ipvs.NewService(&service); err != nil {
+				panic(err)
+			}
+			ipvsService = &service
+		} else {
+			if err := ipvs.UpdateService(&service); err != nil {
+				panic(err)
+			}
 		}
 
-		for _, server := range service.Servers {
-			dst := libipvs.Destination{
+		ipvsDests, err := ipvs.ListDestinations(ipvsService)
+		if err != nil {
+			ltsvlog.Logger.Err(ltsvlog.Err(err).String("msg",
+				"faild get serverIpvs list "+ipvsService.Address.String()).Stack(""))
+		}
+
+		var server ConfigServer
+		for _, server = range serviceConf.Servers {
+			exist = false
+			for _, ipvsDest := range ipvsDests {
+				if ipvsDest.Address.Equal(net.ParseIP(server.Address)) {
+					exist = true
+					pp.Println(ipvsDests)
+					pp.Println(server)
+					break
+				}
+			}
+			dest := libipvs.Destination{
 				Address:       net.ParseIP(server.Address),
 				AddressFamily: syscall.AF_INET,
 				Port:          server.Port,
 				FwdMethod:     libipvs.IP_VS_CONN_F_DROUTE,
+				Weight:        server.Weight,
 			}
-
-			if err := ipvs.NewDestination(&svc, &dst); err != nil {
-				panic(err)
+			if exist {
+				err := ipvs.UpdateDestination(ipvsService, &dest)
+				if err != nil {
+					panic(err)
+				}
+			} else {
+				err := ipvs.NewDestination(ipvsService, &dest)
+				if err != nil {
+					panic(err)
+				}
 			}
 		}
 	}
-
-	svcs, err := ipvs.ListServices()
-	if err != nil {
-		panic(err)
-	}
-	pp.Println(svcs)
 }
