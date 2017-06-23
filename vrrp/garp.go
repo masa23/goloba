cd2ce8c8feea7ff62f4c623afc8e021441961f7f
Hiroaki Nakamura <hnakamur@gmail.com>
2017-06-24 06:04:58 +0900

    Remove comment-outed code

--- vrrp/garp.go
+++ vrrp/garp.go
@@ -31,31 +31,6 @@ func InterfaceByIP(ip net.IP) (*net.Interface, error) {
 	return nil, errors.New("interface not found")
 }
 
-// SendGarp sends a gratuitous ARP packet for the IP address.
-//func SendGARP(ip net.IP) error {
-//	intf, err := InterfaceByIP(ip)
-//	if err != nil {
-//		return err
-//	}
-//
-//	c, err := arp.Dial(intf)
-//	if err != nil {
-//		return err
-//	}
-//	defer c.Close()
-//
-//	p, err := arp.NewPacket(arp.OperationRequest, intf.HardwareAddr, ip,
-//		net.HardwareAddr{0, 0, 0, 0, 0, 0}, ip)
-//	if err != nil {
-//		return err
-//	}
-//	err = c.WriteTo(p, net.HardwareAddr{0xff, 0xff, 0xff, 0xff, 0xff, 0xff})
-//	if err != nil {
-//		return err
-//	}
-//	return nil
-//}
-
 func SendGARP(c *arp.Client, intf *net.Interface, ip net.IP) error {
 	p, err := arp.NewPacket(arp.OperationRequest, intf.HardwareAddr, ip,
 		net.HardwareAddr{0xff, 0xff, 0xff, 0xff, 0xff, 0xff}, ip)
